23be1c85c876e25636c21233213db5d9eeadfa17
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-07-23 17:51:01 +0530

    changes done

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6g89fcmogjo"
+    "revision": "0.c0q9o52u5h"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
