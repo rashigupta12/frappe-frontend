ae8f5d13e7daa501360b6cfa7349d13ea63ad6ec
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-07-25 15:14:30 +0530

    changes

--- src/components/JobCard/JobCardDetails.tsx
+++ src/components/JobCard/JobCardDetails.tsx
@@ -69,7 +69,7 @@ const JobCardDetails: React.FC<Props> = ({ card, onClose }) => {
               'no_of_sides',
               'price',
               'amount',
-              'remarks',
+              
             ]}
           />
         )}
@@ -88,7 +88,7 @@ const JobCardDetails: React.FC<Props> = ({ card, onClose }) => {
               'no_of_sides',
               'price',
               'amount',
-              'remarks',
+              
             ]}
           />
         )}
