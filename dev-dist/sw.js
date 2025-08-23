e549fd52d50fcb080acb620f398356632cbf4b58
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-08-23 16:21:42 +0530

    apple touch

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,11 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-<<<<<<< HEAD
-    "revision": "0.1hpr41bfto"
-=======
-    "revision": "0.lutu25u7llg"
->>>>>>> audio-implemented
+    "revision": "0.uavriv6b2qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
